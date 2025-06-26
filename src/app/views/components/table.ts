fb300559a0fefc005cd42f22fd2053efd24f7488
mjmmattoni98 <javier.magni@gmail.com>
2025-06-26 21:23:00 +0200

    Add table sorting

--- src/app/views/components/table.ts
+++ src/app/views/components/table.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
-import { MatTableModule, MatTableDataSource } from '@angular/material/table';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 
 interface Candidate {
   name: string;
@@ -13,31 +14,71 @@ interface Candidate {
 @Component({
   selector: 'candidates-table',
   standalone: true,
-  imports: [MatTableModule, MatPaginatorModule],
+  imports: [MatTableModule, MatPaginatorModule, MatSortModule],
   template: `
-    <table mat-table [dataSource]="candidates" class="mat-elevation-z8">
+    <table mat-table [dataSource]="candidates" matSort class="mat-elevation-z8">
       <ng-container matColumnDef="name">
-        <th mat-header-cell *matHeaderCellDef>Name</th>
+        <th
+          mat-header-cell
+          *matHeaderCellDef
+          mat-sort-header
+          sortActionDescription="Sort by name"
+        >
+          Name
+        </th>
         <td mat-cell *matCellDef="let candidate">{{ candidate.name }}</td>
       </ng-container>
+
       <ng-container matColumnDef="surname">
-        <th mat-header-cell *matHeaderCellDef>Surname</th>
+        <th
+          mat-header-cell
+          *matHeaderCellDef
+          mat-sort-header
+          sortActionDescription="Sort by surname"
+        >
+          Surname
+        </th>
         <td mat-cell *matCellDef="let candidate">{{ candidate.surname }}</td>
       </ng-container>
+
       <ng-container matColumnDef="seniority">
-        <th mat-header-cell *matHeaderCellDef>Seniority</th>
+        <th
+          mat-header-cell
+          *matHeaderCellDef
+          mat-sort-header
+          sortActionDescription="Sort by seniority"
+        >
+          Seniority
+        </th>
         <td mat-cell *matCellDef="let candidate">{{ candidate.seniority }}</td>
       </ng-container>
+
       <ng-container matColumnDef="years">
-        <th mat-header-cell *matHeaderCellDef>Years</th>
+        <th
+          mat-header-cell
+          *matHeaderCellDef
+          mat-sort-header
+          sortActionDescription="Sort by years"
+        >
+          Years
+        </th>
         <td mat-cell *matCellDef="let candidate">{{ candidate.years }}</td>
       </ng-container>
+
       <ng-container matColumnDef="availability">
-        <th mat-header-cell *matHeaderCellDef>Availability</th>
+        <th
+          mat-header-cell
+          *matHeaderCellDef
+          mat-sort-header
+          sortActionDescription="Sort by availability"
+        >
+          Availability
+        </th>
         <td mat-cell *matCellDef="let candidate">
           {{ candidate.availability ? 'Available' : 'Not Available' }}
         </td>
       </ng-container>
+
       <tr mat-header-row *matHeaderRowDef="displayedColumns"></tr>
       <tr mat-row *matRowDef="let row; columns: displayedColumns"></tr>
     </table>
@@ -45,16 +86,26 @@ interface Candidate {
     <mat-paginator
       [pageSizeOptions]="[5, 10, 20]"
       showFirstLastButtons
-      aria-label="Select page of periodic elements"
+      aria-label="Select page of candidates"
     />
   `,
-  styles: `table { width: 100%; margin: 2rem auto; }`,
+  styles: `
+    table { 
+      width: 100%; 
+      margin: 2rem auto; 
+    }
+    
+    th.mat-sort-header-sorted {
+      color: black;
+    }
+  `,
 })
 export class Table implements OnInit, AfterViewInit {
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
   displayedColumns = ['name', 'surname', 'seniority', 'years', 'availability'];
   candidates = new MatTableDataSource<Candidate>([]);
-
-  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   ngOnInit() {
     this.loadCandidates();
@@ -64,6 +115,7 @@ export class Table implements OnInit, AfterViewInit {
 
   ngAfterViewInit() {
     this.candidates.paginator = this.paginator;
+    this.candidates.sort = this.sort;
   }
 
   loadCandidates() {
